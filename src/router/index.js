063471ce0bedc4655da35bb2882676e51ba22df2
MinorN <1481700559@qq.com>
2022-03-20 22:35:18 +0800

    init

---
+++ src/router/index.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from '../components/login.vue'
+import NotebookList from '../components/NotebookList.vue'
+import NoteDetail from '../components/NoteDetail.vue'
+import TrashDetail from '../components/TrashDetail'
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/login',
+      component: Login
+    },
+    {
+      path: '/notebooks',
+      component: NotebookList
+    },
+    {
+      path: '/note/:noteId',
+      component: NoteDetail
+    },
+    {
+      path: '/trash/:noteId',
+      component: TrashDetail
+    },
+  ]
+})
