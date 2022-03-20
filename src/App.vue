d314bf2b8c7ecdd0502c9d68018dc551afdbc977
MinorN <1481700559@qq.com>
2022-03-20 22:39:40 +0800

    完成sidebar组件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,64 @@
 <template>
   <div id="app">
+    <sidebar/>
     <router-view/>
   </div>
 </template>
 
 <script>
+import sidebar from './components/Slidebar.vue'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    sidebar
+  }
 }
 </script>
 
 <style>
-#app {
+@import '//at.alicdn.com/t/font_496303_kqrjhri8l25d0a4i.css';
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+html, body, #app {
+  height: 100%;
+}
+
+body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-size: 14px;
+
+  background-color: #eee;
+}
+
+a {
+  text-decoration: none;
+  color: #444;
+}
+
+ul, li {
+  list-style: none;
+}
+
+.btn {
+  color: #666;
+  font-size: 12px;
+  padding: 2px 4px;
+  background-color: #fff;
+  box-shadow: 0 0 2px 0 #ccc;
+  border: none;
+  cursor: pointer;
+  display: inline-block;
+}
+
+
+#app {
+  display: flex;
+  align-items: stretch;
 }
 </style>
