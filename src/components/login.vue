cd8c72e1fd4858a832ead679e9b37eef67f9be93
MinorN <1481700559@qq.com>
2022-03-23 23:15:48 +0800

    登陆、注册、注销后页面跳转

--- src/components/login.vue
+++ src/components/login.vue
@@ -41,7 +41,7 @@
 import Auth from "../apis/auth";
 
 Auth.getInfo()
-  .then(data=>{
+  .then(data => {
     console.log(data);
   })
 
@@ -87,14 +87,17 @@ export default {
           this.register.notice = '密码长度为6~16个字符'
           return
         }
-        this.register.isError = false
-        this.register.notice = ''
         Auth.register({
-          username:this.register.username,
-          password:this.register.password
-        }).then(data=>{
-            console.log(data);
-          })
+          username: this.register.username,
+          password: this.register.password
+        }).then(data => {
+          this.register.isError = false
+          this.register.notice = ''
+          this.$router.push({path: 'notebooks'})
+        }).catch(data=>{
+          this.register.isError = true
+          this.register.notice = data.msg
+        })
       },
       onLogin() {
         if (!/^[\w\u4e00-\u9fa5]{3,15}$/.test(this.login.username)) {
@@ -107,13 +110,16 @@ export default {
           this.login.notice = '密码长度为6~16个字符'
           return
         }
-        this.login.isError = false
-        this.login.notice = ''
         Auth.login({
-          username:this.login.username,
-          password:this.login.password
-        }).then(data=>{
-          console.log(data);
+          username: this.login.username,
+          password: this.login.password
+        }).then(data => {
+          this.login.isError = false
+          this.login.notice = ''
+          this.$router.push({path: 'notebooks'})
+        }).catch(data => {
+          this.login.isError = true
+          this.login.notice = data.msg
         })
       }
     }
